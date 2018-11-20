3e45db1cecb5cf8cc790ac7ee33ddf2cb1a0f7c7
lacey <hiu@example.com>
2018-11-20 16:49:35 +0000

    complete 4th test

--- specs/ruby_functions_spec.rb
+++ specs/ruby_functions_spec.rb
@@ -18,11 +18,11 @@ class FunctionsTest < MiniTest::Test
     subtract_result = subtract( 10, 5 )
     assert_equal( 5, subtract_result )
   end
-  #
-  # def test_multiply()
-  #   multiply_result = multiply( 4, 2 )
-  #   assert_equal( 8, multiply_result )
-  # end
+
+  def test_multiply()
+    multiply_result = multiply( 4, 2 )
+    assert_equal( 8, multiply_result )
+  end
   #
   # def test_divide()
   #   divide_result = divide( 10, 2 )
