1631d5ec224271d5a0b7749f8fd8cad3f1651726
lacey <hiu@example.com>
2018-11-20 21:10:27 +0000

    complete test 12

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -11,16 +11,16 @@ def subtract(a, b)
 end
 
 def multiply(a, b)
-  return a*b
+  return a * b
 end
 
 def divide(a, b)
-  return a/b
+  return a / b
 end
 
-def length_of_string(length)
-  return "Abcdefghijklmnopqrstu".length
-end
+def length_of_string(string_length_to_check)
+  return string_length_to_check.length
+end #remember the argument is what the action is done on
 
 def join_string(string_1, string_2)
   return string_1 + string_2
@@ -38,4 +38,9 @@ def number_to_full_month_name(num)
   elsif num == 9
     return "September"
   end
+end
+
+def number_to_short_month_name(num)
+  long_month = number_to_full_month_name(num)
+  return long_month[0..2]
 end
